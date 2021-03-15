504cb6fff9fcb1ee88637b2dc9b19df62e33bc54
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-15 11:56:38 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_username=(username)
+    self.user.username = User.find_by(username: username)
+  end
+
+  def user_username
+    self.user ? self.user.username : nil
+  end
 end
