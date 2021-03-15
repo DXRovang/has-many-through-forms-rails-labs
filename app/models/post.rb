ca61520c2820b4a85b457be28328f971922fd4be
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-15 15:22:22 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
-  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :categories, reject_if: proc { |att| att['name'].blank? }
+  # accepts_nested_attributes_for :comments
+  # accepts_nested_attributes_for :users
 
   has_many :comments
   has_many :users, through: :comments
