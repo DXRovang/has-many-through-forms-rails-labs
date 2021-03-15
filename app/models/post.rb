7488df22d6988348dd8504a0acd1f57c07d94534
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-15 15:54:38 -0400

    commit tree 5bc7ea892207f34dea83cb72b310b8a51aec7290

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,16 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
-  accepts_nested_attributes_for :categories, reject_if: proc { |att| att['name'].blank? }
-  # accepts_nested_attributes_for :comments
-  # accepts_nested_attributes_for :users
-
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
-  def category_ids=(ids)
-    ids.each do |id|
-      category = Category.find(id)
-      self.categories << category
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute["name"].present?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
     end
   end
- 
-
 end
